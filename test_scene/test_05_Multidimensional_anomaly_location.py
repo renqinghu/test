96bb699b6616a45c0f4deb29215220788451b4d2
renqinghu <https://github.com/renqinghu/bizsser_test.git>
2019-10-21 13:33:31 +0800

    update

--- test_scene/test_05_Multidimensional_anomaly_location.py
+++ test_scene/test_05_Multidimensional_anomaly_location.py
@@ -7,7 +7,7 @@ import time
 
 class TestMultiEvent(object):
     """
-    多维度异常定位
+    多维度异常定位 volcano
     """
     node_key = "node.bztest001"
     modigynode_key = "node.bztest002"
@@ -24,16 +24,3 @@ class TestMultiEvent(object):
         assert jobs["code"] == 202
         assert jobs["msg"] == "Accepted"
         assert len(jobs["data"]) != 0
-
-
-    # def test_01_postnodedata(self) -> object:
-    #     '''模块-获取node data [put] /api/task/volcano'''
-    #     url = config.base_url + "/api/task/volcano"
-    #     headers = {'Content-Type': 'application/json'}
-    #     data = {"details": {"events": [{"event_id": "5da6b5421a870b4165bb7bcc", "time": 1524689580}]}}
-    #     r = requests.put(url, data=json.dumps(data), headers=headers)
-    #     jobs = r.json()
-    #     config.pprint(url, jobs, data)
-    #     assert jobs["code"] == 202
-    #     assert jobs["msg"] == "Accepted"
-    #     assert len(jobs["data"]) != 0
